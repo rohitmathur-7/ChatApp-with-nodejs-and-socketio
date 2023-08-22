6e9ddc76fb63126ab16edbc0bb61d3a567e0d939
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2023-08-22 23:53:50 +0530

    Made UI changes

--- js/client.js
+++ js/client.js
@@ -2,8 +2,11 @@ const socket = io("http://localhost:8000", { transports: ["websocket"] });
 const form = document.getElementById("send-container");
 const msgInp = document.getElementById("msgInp");
 const msgContainer = document.querySelector(".container");
-const userNameForm = document.getElementById('userNameForm');
-const userNameInp = document.getElementById('userName');
+const userNameForm = document.getElementById("userNameForm");
+const userNameInp = document.getElementById("userName");
+const chat = document.querySelector(".main-chat");
+const userNameBox = document.querySelector(".name-inp");
+const mainContainer = document.querySelector(".main-container");
 
 const append = (message, position) => {
   const msgElement = document.createElement("div");
@@ -13,12 +16,15 @@ const append = (message, position) => {
   msgContainer.appendChild(msgElement);
 };
 
-userNameForm.addEventListener("submit",(e)=>{
+userNameForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const userName = userNameInp.value;
+  chat.style.display = "block";
+  userNameBox.style.display = "none";
+  mainContainer.style.justifyContent = "space-between";
+
   socket.emit("new-user-joined", userName);
 });
-
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
