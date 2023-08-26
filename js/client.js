f801fb860316fde68bb5097dbea1d0f5aeb93c50
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2023-08-26 14:17:09 +0530

    Made UI Changes: 1)Completely refactored CSS 2)Added fluid typography

--- js/client.js
+++ js/client.js
@@ -4,7 +4,7 @@ const msgInp = document.getElementById("msgInp");
 const msgContainer = document.querySelector(".container");
 const userNameForm = document.getElementById("userNameForm");
 const userNameInp = document.getElementById("userName");
-const chat = document.querySelector(".main-chat");
+const mainChat = document.querySelector(".main-chat");
 const userNameBox = document.querySelector(".name-inp");
 const mainContainer = document.querySelector(".main-container");
 
@@ -19,7 +19,11 @@ const append = (message, position) => {
 userNameForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const userName = userNameInp.value;
-  chat.style.display = "block";
+  mainChat.style.display = "flex";
+  mainChat.style.flexDirection = "column";
+  mainChat.style.width = "80%";
+  mainChat.style.margin = "0 auto";
+  mainChat.style.rowGap = "50px";
   userNameBox.style.display = "none";
   mainContainer.style.justifyContent = "space-between";
 
@@ -29,19 +33,19 @@ userNameForm.addEventListener("submit", (e) => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = msgInp.value;
-  append(`You: ${message}`, "right");
+  append(`You: ${message}`, "msg-right");
   socket.emit("send", message);
   msgInp.value = "";
 });
 
 socket.on("user-joined", (name) => {
-  append(`${name} joined the chat`, "right");
+  append(`${name} joined the chat`, "msg-right");
 });
 
 socket.on("receive", (data) => {
-  append(`${data.userName}: ${data.message}`, "left");
+  append(`${data.userName}: ${data.message}`, "msg-left");
 });
 
 socket.on("left", (name) => {
-  append(`${name} left the chat`, "left");
+  append(`${name} left the chat`, ",msg-left");
 });
